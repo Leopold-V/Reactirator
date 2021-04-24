15aa811a7c5c2731cb9a1affaa22ba2c4055858e
Leopold-V <leopoldV79@gmail.com>
2021-04-24 17:05:56 +0200

    added loading and react toaster

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 const { ipcRenderer } = require('electron');
+import { toast } from 'react-hot-toast';
 
 import initialState from '../helpers/initialState';
 import { generateProject } from '../services/installation';
@@ -17,7 +18,17 @@ export const Form = ({loading, setLoading} : {loading: boolean, setLoading: any}
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (!validateInput(input.appname)) {
-            console.log('pas cool');
+            toast('The project name is invalid !',
+                {
+                    icon: '❌',
+                    style: {
+                        margin: '16px',
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                }
+            );
         } else {
             ipcRenderer.send('open-directory', input); 
         }
@@ -28,7 +39,30 @@ export const Form = ({loading, setLoading} : {loading: boolean, setLoading: any}
             const [filepath, input] = arg;
             if (arg) {
                 setLoading(true);
-                await generateProject(filepath, input);
+                await toast.promise(
+                    generateProject(filepath, input),
+                    {
+                      loading: 'Installation...',
+                      success: () => `Successfully installed !`,
+                      error: (err) => `An error happened: ${err.toString()}`
+                    },
+                    {
+                        style: {
+                            margin: '16px',
+                            borderRadius: '10px',
+                            background: '#333',
+                            color: '#fff',
+                        },
+                        success: {
+                            duration: 5000,
+                            icon: '✅',
+                        },
+                        error: {
+                            duration: 5000,
+                            icon: '❌',
+                        },
+                    }
+                  );
                 setLoading(false);
             }
           });
@@ -38,8 +72,7 @@ export const Form = ({loading, setLoading} : {loading: boolean, setLoading: any}
     }, [])
 
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col items-center mb-10 space-y-6">
-            <h2 className="font-bold text-xl py-6">Generator form :</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center py-6 space-y-6">
             <div className="flex flex-row justify-between items-center w-full">
                 <label className="font-bold pr-4" htmlFor="appname">Project name :</label>
                 <input onChange={handleChange} value={input.appname} className="py-2 px-4 rounded-sm border-gray-300 border-2" 
@@ -49,7 +82,6 @@ export const Form = ({loading, setLoading} : {loading: boolean, setLoading: any}
                     placeholder="Application name" 
                 />
             </div>
-            
             <div className="w-full border-gray-200 border-t-2">
                 <h3 className="font-bold text-center py-4">Syntax :</h3>
                 <div className="flex flex-wrap space-x-6 justify-center">
@@ -72,13 +104,22 @@ export const Form = ({loading, setLoading} : {loading: boolean, setLoading: any}
                 <h3 className="font-bold text-center py-4">Packages :</h3>
                 <div className="flex flex-wrap space-x-6 justify-center">
                     <Checkbox name="reactrouter" setInput={setInput} input={input}>react-router-dom : </Checkbox>
-                    <Checkbox name="propstype" setInput={setInput} input={input}>Props-type : </Checkbox>
+                    <Checkbox name="proptypes" setInput={setInput} input={input}>Props-type : </Checkbox>
                 </div>
             </div>
-
-            <button className="bg-blue-500 px-4 py-2 mt-10 font-semibold tracking-wider text-white rounded hover:bg-blue-700 transition duration-250">
+            {loading ?
+            <button className="bg-red-300 px-4 py-2 font-semibold text-center tracking-wider text-white rounded cursor-not-allowed" disabled>
+                    <svg className="animate-spin -ml-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+            </button>
+            :
+            <button 
+                className="bg-red-500 px-4 py-2 font-semibold tracking-wider text-white rounded hover:bg-red-700 transition duration-250">
                 Create
             </button>
+            }
         </form>
     )
 }
