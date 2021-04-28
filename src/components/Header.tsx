4bd2261dd6aa3e951afb4d206e897d147452d5aa
Leopold-V <leopoldV79@gmail.com>
2021-04-28 22:54:06 +0200

    remove intro

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <header className="py-8 flex flex-col items-center">
-            <h1 className="pb-4 font-bold text-4xl text-center text-white">Create-react-app generator</h1>
-            <p className="text-center">Generate a cra project with your favorite config embedded without touching a shell, everything is automatized so gain time without bothering about config, enjoy.</p>
+            <h1 className="pb-4 font-bold text-4xl text-center text-white">React.js application generator</h1>
         </header>
     )
 }
