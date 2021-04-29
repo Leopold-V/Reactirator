2234ab3b7e48a1d798ccf251a6ff043f1236f93d
Leopold-V <leopoldV79@gmail.com>
2021-04-29 12:17:17 +0200

    added modal title

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,12 +11,13 @@ export const Modal = ({loading, show, toggleModal}: {loading: boolean, show: boo
         <div className="absolute w-screen h-screen flex justify-center items-center top-0 left-0">
             <div className="absolute bg-gray-700 opacity-60 w-screen h-screen top-0 left-0"></div>
             <div className="bg-white rounded z-50 flex flex-col justify-center items-center p-6 lg:w-1/3">
-                <svg xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-settings ${loading ? 'animate-spin-slow' : ''}`} width="150" height="150" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <h2 className="text-xl text-black font-bold py-4">Installation...</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-settings ${loading ? 'animate-spin-slow' : ''}`} width="130" height="130" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
                     <circle cx="12" cy="12" r="3" />
                 </svg>
-                <div className="bg-black rounded overflow-auto h-96 my-6 lg:w-4/5">
+                <div className="bg-black rounded overflow-auto h-80 my-6 lg:w-4/5">
                     <ConsoleLogs />
                 </div>
                 {loading ?
