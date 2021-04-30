98717a03ce9f076b637bca6c245a14185fd955bc
Leopold-V <leopoldV79@gmail.com>
2021-04-30 10:53:34 +0200

    added typescript to package reducer

--- src/reducers/packageReducer.ts
+++ src/reducers/packageReducer.ts
@@ -22,7 +22,6 @@ const packageReducer = (state: any, { type, payload }: {type: string, payload: {
             state.devDependencies['autoprefixer'] = "^9.8.6";
             state.devDependencies['postcss'] = "^7.0.35";
             state.devDependencies['tailwindcss'] = "npm:@tailwindcss/postcss7-compat@^2.1.2";
-            return {...state};
           } else {
             delete state.dependencies['@craco/craco'];
             state.scripts.start = 'npm start';
@@ -32,8 +31,8 @@ const packageReducer = (state: any, { type, payload }: {type: string, payload: {
             delete state.devDependencies['autoprefixer'];
             delete state.devDependencies['postcss'];
             delete state.devDependencies['tailwindcss'];
-            return {...state};
           }
+          return {...state};
         case 'storybook':
           if (!state.scripts['storybook']) {
             state.scripts['storybook'] = "start-storybook -p 6006 -s public";
@@ -48,7 +47,6 @@ const packageReducer = (state: any, { type, payload }: {type: string, payload: {
             state.devDependencies["@storybook/preset-create-react-app"] = "^^3.1.7";
             state.devDependencies["@storybook/addon-actions"] = "^6.2.9";
             state.devDependencies["@storybook/react"] = "^6.2.9";
-            return {...state};
           } else {
             delete state.scripts['storybook'];
             delete state.scripts['build-storybook'];
@@ -59,50 +57,60 @@ const packageReducer = (state: any, { type, payload }: {type: string, payload: {
             delete state.devDependencies["@storybook/preset-create-react-app"]
             delete state.devDependencies["@storybook/addon-actions"]
             delete state.devDependencies["@storybook/react"]
-            return {...state};
           }
-        case 'prettier': {
+          return {...state};
+        case 'prettier':
           if (!state.devDependencies?.prettier) {
             if (!state.devDependencies) {
               state.devDependencies = {};
             }
             state.devDependencies["prettier"] = "2.2.1";
-            return {...state};
           } else {
             delete state.devDependencies["prettier"];
-            return {...state};
           }
-        }
-        case 'flow': {
+          return {...state};
+        case 'flow':
           if (!state.dependencies['flow']) {
             state.dependencies['flow-bin'] = "^0.150.0";
             state.scripts['flow'] = 'flow';
-            return {...state};
           } else {
             delete state.dependencies['flow-bin'];
-            return {...state};
           }
-        }
+          return {...state};
         case 'bootstrap': {
           if (!state.dependencies['bootstrap']) {
             state.dependencies['bootstrap'] = "^4.6.0";
-            return {...state};
           } else {
             delete state.dependencies['bootstrap'];
-            return {...state};
           }
+          return {...state};
         }
         case 'sourcemapexplorer': {
           if (!state.dependencies['source-map-explorer']) {
             state.dependencies['source-map-explorer'] = "^2.5.2";
             state.scripts['analyze'] = "source-map-explorer 'build/static/js/*.js'";
-            return {...state};
           } else {
             delete state.dependencies['flow-bin'];
-            return {...state};
           }
+          return {...state};
+        }
+        case 'normalize': {
+          return {...state};
         }
         case 'typescript': {
+          if (!state.dependencies['typescript']) {
+            state.dependencies['typescript'] = "^4.2.4";
+            state.dependencies['@types/jest'] ="^26.0.23";
+            state.dependencies['@types/node'] = "^12.20.11";
+            state.dependencies['@types/react'] = "^17.0.4";
+            state.dependencies['@types/react-dom'] = "^17.0.3";
+          } else {
+            delete state.dependencies['typescript']
+            delete state.dependencies['@types/jest']
+            delete state.dependencies['@types/node']
+            delete state.dependencies['@types/react']
+            delete state.dependencies['@types/react-dom']
+          }
           return {...state};
         }
         default:
