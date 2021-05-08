7b9639e551449882aae7841907f1b0ccb890df72
Leopold-V <leopoldV79@gmail.com>
2021-05-08 09:36:49 +0200

    remove the dialog window callback

--- src/components/ButtonCreation.tsx
+++ src/components/ButtonCreation.tsx
@@ -1,43 +1,20 @@
 import React from 'react'
 import toast from 'react-hot-toast';
-import { ipcRenderer, remote } from 'electron';
-const { dialog } = remote;
+import { ipcRenderer } from 'electron';
 
 import { toastValidationStyle } from '../helpers/toast';
 import validateInput from '../utils/validate_input';
 import { formInputType } from '../helpers/types';
 
-const dialogOptions = {
-    type: 'question',
-    buttons: ['Yes', "No"],
-    defaultId: 1,
-    cancelId: 1,
-    title: 'Are you sure ?',
-    message:
-      'The installation process is about to begin.',
-    detail:
-      "It might take a couple of minutes after you choose your project location.",
-};
-
 export const ButtonCreation = ({input} : {input: formInputType}) => {
-
-    //const win = remote.getCurrentWindow();
-
-    const dialogCallback = (response: number): void => {
-        const isConfirmed = response === 0;
-        if (isConfirmed) {
-            if (!validateInput(input.appname)) {
-                toast('The project name is invalid !', toastValidationStyle);
-            } else {
-            ipcRenderer.send('open-directory', input); 
-            }
-        }
-    }
 
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
         e.preventDefault();
-        const result = await dialog.showMessageBox(dialogOptions);
-        dialogCallback(result.response);
+        if (!validateInput(input.appname)) {
+            toast('The project name is invalid !', toastValidationStyle);
+        } else {
+        ipcRenderer.send('open-directory', input); 
+        }
     };
 
     return (
