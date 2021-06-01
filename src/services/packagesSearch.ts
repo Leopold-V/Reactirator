ecb449320214c6c8f184be86ee61f2de78c94667
Leopold-V <leopoldV79@gmail.com>
2021-06-01 15:08:08 +0200

    refactored the registry fetching

--- src/services/packagesSearch.ts
+++ src/services/packagesSearch.ts
@@ -1,4 +1,7 @@
+const request = require('request');
+
 const API_URL = 'https://api.npms.io/v2/search?q=';
+const REGISTRY_URL = 'https://registry.npmjs.org';
 
 export const getPackages = async (packageName: string, size = 30): Promise<any> => {
     try {
@@ -9,3 +12,31 @@ export const getPackages = async (packageName: string, size = 30): Promise<any> 
         throw error;
     }
 }
+
+export const getPackageRegistryInfo = (name: string, version: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      request(
+        { url: `${REGISTRY_URL}/${name}/${version}`, json: true },
+        (err: Error, res: any) => {
+          if (!err) {
+            let status = null;
+            switch (res.statusCode) {
+              case 400:
+                status = 'Invalid data';
+                break;
+              case 404:
+                status = 'Package not found';
+                break;
+              case 412:
+                status = 'Precondition failed';
+                break;
+            }
+            resolve(res.body);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+  };
+  
