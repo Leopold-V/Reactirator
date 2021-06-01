856bd2c69c935e6c854cb12128e8c99bcdf8ccb6
Leopold-V <leopoldV79@gmail.com>
2021-06-01 15:07:48 +0200

    remove useless line

--- src/components/Treemap.tsx
+++ src/components/Treemap.tsx
@@ -1,27 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { generateTreeMapWithD3 } from '../utils/generateTreeMapWithD3';
 import { depStateType } from '../helpers/types';
-import listDepsSize from '../utils/listDepsSize';
 
-export const Treemap = ({ listPackages, totalSize }: { listPackages: depStateType, totalSize: number }) => {
-    const [depsSize, setDepsSize] = useState(0);
-    const [devDepsSize, setDevDepsSize] = useState(0);
+export const Treemap = ({ listPackages }: { listPackages: depStateType }) => {
   
     useEffect(() => {
-      setDepsSize(listDepsSize(listPackages.dependencies));
-      setDevDepsSize(listDepsSize(listPackages.devDependencies));
       generateTreeMapWithD3(listPackages);
     }, [listPackages]);
 
     return (
         <div className="w-8/12 mx-auto">
             <div className="p-6 bg-white text-gray-800 rounded shadow flex justify-center">
-                <div id="treemap">
-                    <svg id="svg_pie" width="400" height="240" />
-                </div> 
+                <div id="treemap"></div> 
             </div>
         </div>
     )
 }
 
-export const PackagesSizeMemoized = React.memo(Treemap);
+export const TreemapMemoized = React.memo(Treemap);
