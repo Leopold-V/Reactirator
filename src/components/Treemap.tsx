25c0187f4196c4e3132de253cbfbf228df3364ba
Leopold-V <leopoldV79@gmail.com>
2021-06-02 08:11:10 +0200

    added title

--- src/components/Treemap.tsx
+++ src/components/Treemap.tsx
@@ -11,6 +11,7 @@ export const Treemap = ({ listPackages }: { listPackages: depStateType }) => {
     return (
         <div className="w-8/12 mx-auto">
             <div className="p-6 bg-white text-gray-800 rounded shadow flex justify-center">
+                <h2>Treemap</h2>
                 <div id="treemap"></div> 
             </div>
         </div>
