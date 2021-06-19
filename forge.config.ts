3cda9d9153e088f35e656f132b7cf7d7c9682a4e
Leopold-V <leopoldV79@gmail.com>
2021-06-19 19:19:01 +0200

    fix github actions

--- forge.config.ts
+++ forge.config.ts
@@ -2,6 +2,7 @@ module.exports = {
     publishers: [
         {
             name: "@electron-forge/publisher-github",
+            platforms: ['darwin', 'linux', 'win32'],
             config: {
             repository: {
                 owner: "Leopold-V",
