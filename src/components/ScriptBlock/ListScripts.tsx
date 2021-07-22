417c0d2a80af137c967c699668a2186fac69bdea
Leopold-V <leopoldV79@gmail.com>
2021-07-22 11:45:33 +0200

    fix missing key

--- src/components/ScriptBlock/ListScripts.tsx
+++ src/components/ScriptBlock/ListScripts.tsx
@@ -4,8 +4,8 @@ import { ScriptItem } from './ScriptItem';
 export const ListScripts = ({ scripts }: { scripts: {} }) => {
   return (
     <ul className="text-sm flex flex-wrap justify-center items-center">
-      {Object.entries(scripts).map((script: any) => {
-        return <ScriptItem script={script} />;
+      {Object.entries(scripts).map((script: any, i) => {
+        return <ScriptItem key={i} script={script} />;
       })}
     </ul>
   );
