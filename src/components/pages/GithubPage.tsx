cb10b88a9f30333ea44d09b592fb65fff9eba6be
Leopold-V <leopoldV79@gmail.com>
2021-08-30 12:25:28 +0200

    feat: create repo

--- src/components/pages/GithubPage.tsx
+++ src/components/pages/GithubPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { authGitHub, getToken } from '../../utils/authGithub';
+import { createRepo, getToken } from '../../services/github.services';
+import { authGitHub } from '../../utils/authGithub';
 
 export const GithubPage = () => {
   const [token, setToken] = useState(null);
@@ -13,8 +14,9 @@ export const GithubPage = () => {
     setToken(token);
   }
 
-  console.log(process.env.CLIENT_ID);
-  console.log(process.env.CLIENT_SECRET);
+  const handleCreateRepo = () => {
+    createRepo(token, 'api_test');
+  }
 
   if (loading) return <div>Authentication...</div>
   return (
@@ -31,7 +33,7 @@ export const GithubPage = () => {
               <path d="M12 2A10 10 0 002 12a10 10 0 006.8 9.5c.5 0 .7-.2.7-.5v-1.7C6.7 20 6.1 18 6.1 18c-.4-1.2-1-1.5-1-1.5-1-.6 0-.6 0-.6 1 0 1.5 1 1.5 1 .9 1.6 2.4 1.1 3 .9 0-.7.3-1.1.6-1.4-2.2-.2-4.6-1-4.6-4.9 0-1.1.4-2 1-2.7 0-.3-.4-1.3.2-2.7 0 0 .8-.2 2.7 1a9.4 9.4 0 015 0c2-1.2 2.8-1 2.8-1 .5 1.4.1 2.4 0 2.7.7.7 1 1.6 1 2.7 0 3.8-2.3 4.7-4.5 5 .4.2.7.8.7 1.8V21c0 .3.2.6.7.5 4-1.3 6.8-5 6.8-9.5A10 10 0 0012 2z"></path>
             </svg>
         </button>
-        : <div>You are connected to github !</div>}
+        : <button onClick={handleCreateRepo}>Create repo ?</button>}
       </div>
     </div>
   );
---
+++ src/services/github.services.ts
@@ -0,0 +1,50 @@
+import { AuthOptions } from "../helpers/types";
+import { Constants } from "../helpers/gitServicesOptions";
+
+const PROXY_URL = 'https://cors.bridged.cc'; // use to prevent cors blockage when fetching github access_token endpoint from client side.
+
+export const createRepo = (token: string, repo_name: string) => {
+    fetch("https://api.github.com/user/repos", {
+        method: 'POST',
+        headers: {
+            "Authorization": token,
+            "Accept": "application/vnd.github.v3+json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: JSON.stringify({name: 'test_api_creation'})
+    })
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+};
+
+
+export const getToken = async (
+    authCode: string,
+    authOptions: AuthOptions = Constants.DEFAULT_AUTH_OPTIONS
+  ): Promise<any> => {
+    
+    const url = `${PROXY_URL}/https://${authOptions.hostname}/login/oauth/access_token`;
+    const data = {
+      client_id: authOptions.clientId,
+      client_secret: authOptions.clientSecret,
+      code: authCode,
+    };
+    try {
+      const response = await fetch(url, {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify(data),
+        });
+      const json = await response.json();
+      return {
+        hostname: authOptions.hostname,
+        token: json.access_token,
+      };
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
