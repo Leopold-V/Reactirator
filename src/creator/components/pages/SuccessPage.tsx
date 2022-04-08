9b84f3d11bd2859876b44cb241fa560243110ff4
Leopold-V <leopoldV79@gmail.com>
2022-04-08 17:33:54 +0200

    feat: open in editor btn

--- src/creator/components/pages/SuccessPage.tsx
+++ src/creator/components/pages/SuccessPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { ArrowLeftIcon } from '@heroicons/react/outline';
+import launchEditor from 'react-dev-utils/launchEditor';
 import { Link } from 'react-router-dom';
+import { ArrowLeftIcon } from '@heroicons/react/outline';
 
-//TODO:
-//Add open in editor button
-export const SuccessPage = () => {
+export const SuccessPage = ({ location }: {location: { pathname: string, state: string }}) => {
+  const projectPath = location.state;
+
+  const redirectToEditor = () => {
+    try {
+      launchEditor(projectPath, 1, 1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div id="layout" className="relative bg-gray-50 h-screen">
       <div className="flex items-center h-full w-full px-8 py-4">
@@ -13,7 +22,28 @@ export const SuccessPage = () => {
           <div className="w-1/3">
             <img src="../assets/undraw_well_done.svg" alt="well_done" />
           </div>
-          <p className="py-6">Your project has been created successfully.</p>
+          <p className="pt-6 pb-2">Your project has been created successfully.</p>
+          <button
+            id="open_project"
+            className="flex items-center space-x-1 cursor-pointer mb-6"
+            onClick={redirectToEditor}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#575c65"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+            <span className="text-sm text-gray-700">Open in editor</span>
+          </button>
           <Link
             to="/"
             className="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
